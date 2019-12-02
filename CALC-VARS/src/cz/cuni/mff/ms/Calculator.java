378592fdd7c29b88b105c01b1d80f1ad5110e31c
Michal Kyjovsky <michalkyjovsky@hotmail.com>
2019-12-02 13:06:32 +0100

    ...

--- CALC-VARS/src/cz/cuni/mff/ms/Calculator.java
+++ CALC-VARS/src/cz/cuni/mff/ms/Calculator.java
@@ -2,30 +2,86 @@ package cz.cuni.mff.ms;
 
 import java.util.ArrayList;
 import java.util.LinkedList;
-import java.util.Queue;
 import java.util.Stack;
 import java.util.concurrent.LinkedBlockingDeque;
 
 public class Calculator {
     public String calculateInput(String input) {
 
-        ArrayList<Double> numbers = new ArrayList<>();
-        ArrayList<String> operators = new ArrayList<>();
-        String output = "";
+        Stack<Double> numbers = new Stack<>();
+        Stack<String> operators = new Stack<>();
+        StringBuilder output = new StringBuilder();
+        boolean first = false;
 
         if(filterBadInput(input)){
             return "ERROR";
         }
         String[] formula = refactorInputLine(input);
-        int deepCounter = 0;
+        double number;
 
-        for (int i = 0; i < formula.length; i++){
-            if(formula[i].equals("(")){
-                deepCounter++;
+        for (String item : formula) {
+            try {
+                number = Double.parseDouble(item);
+                output.append(number + " ");
+            }catch (Exception e) {
+
+                if (!first){
+                    operators.push(item);
+                    first = true;
+                }else {
+                    switch (item.strip()) {
+                        case "+":
+                            if (operators.empty() || operators.peek().equals("(")) {
+                                operators.push("+");
+                            } else {
+                                output.append(operators.pop() + " ");
+                            }
+                            break;
+                        case "-":
+                            if (operators.empty() || operators.peek().equals("(")) {
+                                operators.push("-");
+                            } else {
+                                output.append(operators.pop() + " ");
+                            }
+                            break;
+                        case "*":
+                            if (operators.peek().equals("+") || operators.peek().equals("-") || operators.empty()) {
+                                operators.push("*");
+                            } else {
+                                while (operators.peek().equals("+") || operators.peek().equals("-") || operators.empty()) {
+                                    output.append(operators.pop() + " ");
+                                }
+                            }
+                            break;
+                        case "/":
+                            if (operators.peek().equals("+") || operators.peek().equals("-")) {
+                                operators.push("/");
+                            } else {
+                                while (operators.peek().equals("+") || operators.peek().equals("-")) {
+                                    output.append(operators.pop() + " ");
+                                }
+                            }
+                            break;
+                        case "(":
+                            operators.push("(");
+                            break;
+                        case ")":
+                            while (!operators.peek().equals("(")) {
+                                output.append(operators.pop() + " ");
+                            }
+                            operators.pop();
+                            break;
+                    }
+                }
             }
         }
+        if(!operators.empty()){
+            output.append(operators.pop());
+        }
 
-        return output;
+
+        System.out.println(output.toString());
+        return output.toString();
     }
 
 
@@ -37,15 +93,28 @@ public class Calculator {
         //Input WhiteSpace between all operators if not present
         for(int i = 0; i < input.length(); i++){
             switch (input.charAt(i)){
-                case '+' | '/' | '*' |'(' | ')':
-                    editedInput.append(" " + input.charAt(i));
+                case '+':
+                case '/':
+                case '*':
+                    editedInput.append(" " + input.charAt(i) + " ");
+                    break;
+                case '(':
+                case ')':
+                    int len = editedInput.length();
+                    if(i > 0 && editedInput.toString().charAt(len-1) != ' '){
+                        editedInput.append(" " + input.charAt(i) + " ");
+                    }else {
+                        editedInput.append(input.charAt(i) + " ");
+                    }
                     break;
                 case '-':
                     if(i == 0){
                         editedInput.append(input.charAt(i));
                     }else if(input.substring(0,i).matches(".*\\d.*")){
-                        editedInput.append(" " + input.charAt(i));
+                        editedInput.append(" " + input.charAt(i) + " ");
                     }
+                    break;
+                case ' ':
                     break;
                 default:
                     editedInput.append(input.charAt(i));
